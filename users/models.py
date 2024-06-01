f3dd384d9b47230a070c39af279fca19b64b8a52
Asadbek-1101 <asadbekshahobiddinov01@gmail.com>
2024-06-01 13:49:51 +0500

    ..

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    image = models.ImageField(upload_to='profile_pics/', blank=True, null=True, default='profile_pics/default.jpg')
+    phone_number = models.CharField(max_length=13, blank=True, null=True)
+    address = models.CharField(max_length=200, blank=True, null=True)
+
--- users/views.py
+++ users/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.views import View
+from .forms import LoginForm, RegisterForm
 
-# Create your views here.
+
+class LoginView(View):
+    def get(self, request):
+        form = LoginForm()
+        return render(request, 'users/login.html', {'form': form})
+
+    def post(self, request):
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+
+        form = LoginForm()
+        return render(request, 'users/login.html', {'form': form})
+
+
+class RegisterView(View):
+
+    def get(self, request):
+        form = RegisterForm()
+        return render(request, 'users/register.html', {'form': form})
+
+    def post(self, request):
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+            return redirect('/')
+
+        form = RegisterForm()
+        return render(request, 'users/register.html', {'form': form})
